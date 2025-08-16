f9730ca77c9d18860f65576b19a7ad70e4f30341
AllSungho <nanumeran@gmail.com>
2025-08-17 04:19:07 +0900

    Update phone field and change method in Member class

--- src/main/java/org/example/librarymanagementsystem/Member.java
+++ src/main/java/org/example/librarymanagementsystem/Member.java
@@ -6,17 +6,27 @@ import lombok.Getter;
 public class Member {
 
     private final Long memberId;
-    private final String name;
-    private final Grade grade;
+    private String name;
+    private String phone;
+    private Grade grade;
 
-    private Member(Long memberId, String name, Grade grade) {
+    private Member(Long memberId, String name, String phone, Grade grade) {
         this.memberId = memberId;
         this.name = name;
+        this.phone = phone;
         this.grade = grade;
     }
     // 정적 팩토리 메서드
-    public static Member newInstance(Long memberId, String name, Grade grade) {
-        return new Member(memberId, name, grade);
+    public static Member newInstance(Long memberId, String name, String phone, Grade grade) {
+        return new Member(memberId, name, phone, grade);
     }
-
+    public void changeName(String name) {
+        this.name = name;
+    }
+    public void changePhone(String phone) {
+        this.phone = phone;
+    }
+    public void changeGrade(Grade grade) {
+        this.grade = grade;
+    }
 }
