8759bd125e97cdae4c89c2254ff064023ade2e2c
AllSungho <nanumeran@gmail.com>
2025-08-17 04:17:06 +0900

    Update Librarian class

--- src/main/java/org/example/librarymanagementsystem/Librarian.java
+++ src/main/java/org/example/librarymanagementsystem/Librarian.java
@@ -2,4 +2,23 @@ package org.example.librarymanagementsystem;
 
 // 사서
 public class Librarian {
+
+    private final Long LibrarianId;
+    private String name;
+    private String phone;
+
+    private Librarian(Long librarianId, String name, String phone) {
+        this.LibrarianId = librarianId;
+        this.name = name;
+        this.phone = phone;
+    }
+    public static Librarian getInstance(Long librarianId, String name, String phone) {
+        return new Librarian(librarianId, name, phone);
+    }
+    public void changeName(String name) {
+        this.name = name;
+    }
+    public void changePhone(String phone) {
+        this.phone = phone;
+    }
 }
