323b63b4e81184203bf178cc376eee61c0f93b98
AllSungho <nanumeran@gmail.com>
2025-08-17 04:04:03 +0900

    Update Available

--- src/main/java/org/example/librarymanagementsystem/Available.java
+++ src/main/java/org/example/librarymanagementsystem/Available.java
@@ -1,6 +1,16 @@
 package org.example.librarymanagementsystem;
 
 public enum Available {
+
     BORROWED("B", "빌려감"),
     RETURNED("R", "반납됨"),
+    STORAGE("S", "보관 중");
+
+    private final String code;
+    private final String description;
+
+    Available(String code, String description) {
+        this.code = code;
+        this.description = description;
+    }
 }
