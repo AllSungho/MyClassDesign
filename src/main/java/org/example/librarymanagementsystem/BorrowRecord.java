11a38243eb9ac0d0582359cd43042391d471efc7
AllSungho <nanumeran@gmail.com>
2025-08-17 04:12:27 +0900

    Update Librarian field in BorrowRecord class

--- src/main/java/org/example/librarymanagementsystem/BorrowRecord.java
+++ src/main/java/org/example/librarymanagementsystem/BorrowRecord.java
@@ -9,21 +9,26 @@ public class BorrowRecord {
 
     private final Book book;
     private final Member member;
+    private final Librarian borrowLibrarian;
     private final LocalDate borrowDate;
+    private Librarian returnLibrarian;
     private LocalDate dueDate;
 
-    private BorrowRecord(Book book, Member member) {
+    private BorrowRecord(Book book, Member member, Librarian borrowLibrarian) {
         this.book = book;
         this.member = member;
+        this.borrowLibrarian = borrowLibrarian;
         this.borrowDate = LocalDate.now();
+        this.returnLibrarian = null;
         this.dueDate = null;
     }
     // 정적 팩토리 메서드
-    public static BorrowRecord newInstance(Book book, Member member) {
-        return new BorrowRecord(book, member);
+    public static BorrowRecord newInstance(Book book, Member member, Librarian borrowLibrarian) {
+        return new BorrowRecord(book, member, borrowLibrarian);
     }
 
-    public void returnRecord() {
+    public void returnRecord(Librarian returnLibrarian) {
         this.dueDate = LocalDate.now();
+        this.returnLibrarian = returnLibrarian;
     }
 }
